1783ad55b9409a990bc215b6b35124ae1edc8de6
L8426936 <1278764240@qq.com>
2020-01-04 12:38:46 +0800

    微信好友清理脚本修改为闭包模式，避免变量冲突

--- clear_friends/main.js
+++ clear_friends/main.js
@@ -4,126 +4,131 @@ const CONFIG = require("./config.js");
 const COMMON = require("./common.js");
 const NODE_UTIL = require("./utils/node_util.js");
 const APP_UTIL = require("./utils/app_util.js");
-const ASSERTION_FRIENDS = require("./modules/assertion_friends.js");
-const DELETE_ABNORMAL_FRIENDS = require("./modules/delete_abnormal_friends.js");
 
-ui.layout(
-    <frame padding="8">
-        <text id="support_we_chat" gravity="center"/>
-        <vertical>
-            <list id="list" marginBottom="64">
-                <horizontal padding="0 8">
-                    <checkbox id="single_assertion_friend_checkbox" layout_gravity="center" checked="{{is_delete}}"/>
-                    <vertical>
-                        <text text="{{we_chat_name}}"/>
-                        <text text="{{friend_nickname}}"/>
-                        <text text="{{assertion}}"/>
-                    </vertical>
-                </horizontal>
-            </list>
-        </vertical>
-        <horizontal>
-            <button id="clear_button" layout_weight="1" layout_gravity="bottom" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="清除本地数据"/>
-            <button id="delete_button" layout_weight="1" layout_gravity="bottom" textColor="red" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="开始删除"/>
-            <button id="assertion_button" layout_weight="1" layout_gravity="bottom" textColor="green" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="开始标记"/>
-        </horizontal>
-    </frame>
-);
+(() => {
+    const ASSERTION_FRIENDS = require("./modules/assertion_friends.js");
+    const DELETE_ABNORMAL_FRIENDS = require("./modules/delete_abnormal_friends.js");
+    
+    ui.layout(
+        <frame padding="8">
+            <text id="support_we_chat" gravity="center"/>
+            <vertical>
+                <list id="list" marginBottom="64">
+                    <horizontal padding="0 8">
+                        <checkbox id="single_assertion_friend_checkbox" layout_gravity="center" checked="{{is_delete}}"/>
+                        <vertical>
+                            <text text="{{we_chat_name}}"/>
+                            <text text="{{friend_nickname}}"/>
+                            <text text="{{assertion}}"/>
+                        </vertical>
+                    </horizontal>
+                </list>
+            </vertical>
+            <horizontal>
+                <button id="clear_button" layout_weight="1" layout_gravity="bottom" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="清除本地数据"/>
+                <button id="delete_button" layout_weight="1" layout_gravity="bottom" textColor="red" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="开始删除"/>
+                <button id="assertion_button" layout_weight="1" layout_gravity="bottom" textColor="green" style="Widget.AppCompat.Button.Colored" textStyle="bold" text="开始标记"/>
+            </horizontal>
+        </frame>
+    );
 
-const GONE = 8, VISIBLE = 0;
-var abnormal_friends = {}, is_delete_count = 0;
+    const GONE = 8, VISIBLE = 0;
+    let abnormal_friends, is_delete_count;
 
-function init() {
-    if (auto.service == null) {
-        dialogs.alert("提示", "去开启微信清理好友无障碍服务").then(() => {
-            app.startActivity({
-                action: "android.settings.ACCESSIBILITY_SETTINGS"
+    function init() {
+        if (auto.service == null) {
+            dialogs.alert("提示", "去开启微信清理好友无障碍服务").then(() => {
+                app.startActivity({
+                    action: "android.settings.ACCESSIBILITY_SETTINGS"
+                });
             });
+        }
+
+        ui.support_we_chat.setText("仅支持" + CONFIG.SUPPORT_WE_CHAT_VERSIONS + "版本的微信");
+        let is_support = CONFIG.SUPPORT_WE_CHAT_VERSIONS.includes(APP_UTIL.getAppVersion(CONFIG.WE_CHAT_PACKAGE_NAME));
+        ui.support_we_chat.setVisibility(is_support ? GONE : VISIBLE);
+
+        ui.delete_button.enabled = false;
+        ui.assertion_button.enabled = is_support;
+
+        abnormal_friends = COMMON.getAbnormalFriends();
+        is_delete_count = 0;
+        let list_data = [];
+        for (let we_chat_name of Object.keys(abnormal_friends)) {
+            list_data.push(abnormal_friends[we_chat_name]);
+        }
+        ui.list.setDataSource(list_data);
+        if (list_data.length > 0) {
+            ui.delete_button.enabled = true;
+        }
+    }
+
+    ui.emitter.on("resume", init);
+
+    ui.list.on("item_bind", (itemView, itemHolder) => {
+        itemView.single_assertion_friend_checkbox.on("check", (checked) => {
+            let item = itemHolder.item;
+            is_delete_count = checked ? is_delete_count + 1 : is_delete_count - 1;
+            if (abnormal_friends[item.we_chat_name].is_delete != checked) {
+                abnormal_friends[item.we_chat_name].is_delete = checked;
+                COMMON.putAbnormalFriends(abnormal_friends);
+            }
         });
-    }
-
-    ui.support_we_chat.setText("仅支持" + CONFIG.SUPPORT_WE_CHAT_VERSIONS + "版本的微信");
-    let is_support = CONFIG.SUPPORT_WE_CHAT_VERSIONS.includes(APP_UTIL.getAppVersion(CONFIG.WE_CHAT_PACKAGE_NAME));
-    ui.support_we_chat.setVisibility(is_support ? GONE : VISIBLE);
-
-    ui.delete_button.enabled = false;
-    ui.assertion_button.enabled = is_support;
-
-    abnormal_friends = COMMON.getAbnormalFriends();
-    let list_data = [];
-    for (let we_chat_name of Object.keys(abnormal_friends)) {
-        list_data.push(abnormal_friends[we_chat_name]);
-    }
-    ui.list.setDataSource(list_data);
-    if (list_data.length > 0) {
-        ui.delete_button.enabled = true;
-    }
-}
-
-ui.emitter.on("resume", init);
-
-ui.list.on("item_bind", (itemView, itemHolder) => {
-    itemView.single_assertion_friend_checkbox.on("check", (checked) => {
-        let item = itemHolder.item;
-        is_delete_count = checked ? is_delete_count + 1 : is_delete_count - 1;
-        if (abnormal_friends[item.we_chat_name].is_delete != checked) {
-            abnormal_friends[item.we_chat_name].is_delete = checked;
-            COMMON.putAbnormalFriends(abnormal_friends);
-        }
+        itemView.single_assertion_friend_checkbox.on("click", () => {
+            let item = itemHolder.item;
+            let assertion = item.assertion;
+            let checked = itemView.single_assertion_friend_checkbox.checked;
+            if (checked && assertion != "请确认你和他（她）的好友关系是否正常" && assertion != "你不是收款方好友，对方添加你为好友后才能发起转账") {
+                dialogs.build({
+                    content: "对方没有拉黑或删除你，确定将对方加入待删除名单？",
+                    positive: "确定",
+                    negative: "取消"
+                }).on("positive", () => {
+                }).on("cancel", () => {
+                    itemView.single_assertion_friend_checkbox.checked = false;
+                }).show();
+            }
+        });
     });
-    itemView.single_assertion_friend_checkbox.on("click", () => {
-        let item = itemHolder.item;
-        let assertion = item.assertion;
-        let checked = itemView.single_assertion_friend_checkbox.checked;
-        if (checked && assertion != "请确认你和他（她）的好友关系是否正常" && assertion != "你不是收款方好友，对方添加你为好友后才能发起转账") {
-            dialogs.build({
-                content: "对方没有拉黑或删除你，确定将对方加入待删除名单？",
-                positive: "确定",
-                negative: "取消"
-            }).on("positive", () => {
-            }).on("cancel", () => {
-                itemView.single_assertion_friend_checkbox.checked = false;
-            }).show();
-        }
-    });
-});
 
-ui.clear_button.on("click", () => {
-    dialogs.build({
-        content: "清空数据，包括所有异常好友，已检查过的所有好友",
-        positive: "确定",
-        negative: "取消"
-    }).on("positive", () => {
-        COMMON.putAbnormalFriends({});
-        COMMON.putCheckedFriends({});
-        init();
-    }).on("cancel", () => {
-    }).show();
-});
-
-ui.delete_button.on("click", () => {
-    if (is_delete_count > 0) {
+    ui.clear_button.on("click", () => {
         dialogs.build({
-            title: "危险操作！！！",
-            titleColor: "red",
-            content: "删除选中的好友",
+            content: "清空数据，包括所有的异常好友，已检查过的好友",
             positive: "确定",
             negative: "取消"
         }).on("positive", () => {
-            threads.start(function(){
-                DELETE_ABNORMAL_FRIENDS.main();
-            });
+            COMMON.putAbnormalFriends({});
+            COMMON.putCheckedFriends({});
+            toast("已清空");
+            init();
         }).on("cancel", () => {
         }).show();
-    } else {
-        toast("至少勾选一个复选框");
-    }
-});
-
-ui.assertion_button.on("click", () => {
-    threads.start(function() {
-        ASSERTION_FRIENDS.main();
     });
-});
 
-init();
+    ui.delete_button.on("click", () => {
+        if (is_delete_count > 0) {
+            dialogs.build({
+                title: "危险操作！！！",
+                titleColor: "red",
+                content: "删除选中的好友",
+                positive: "确定",
+                negative: "取消"
+            }).on("positive", () => {
+                threads.start(function(){
+                    DELETE_ABNORMAL_FRIENDS.main();
+                });
+            }).on("cancel", () => {
+            }).show();
+        } else {
+            toast("至少勾选一个复选框");
+        }
+    });
+
+    ui.assertion_button.on("click", () => {
+        threads.start(function() {
+            ASSERTION_FRIENDS.main();
+        });
+    });
+
+    init();
+})();
--- clear_friends/modules/delete_abnormal_friends.js
+++ clear_friends/modules/delete_abnormal_friends.js
@@ -2,159 +2,160 @@
  * 删除好友
  */
 
-/**
- * @var abnormal_friends 异常好友
- * @var last_we_chat_name 上一次点击的好友的微信号
- * @var last_friend_nickname 上一次点击的好友的昵称
- * @var last_index 上一次点击的可见好友的位置
- */
-var abnormal_friends, deleted_abnormal_friends, last_index, step, run;
+module.exports = (() => {
+    /**
+     * @var abnormal_friends 异常好友
+     * @var deleted_abnormal_friends 已删除的好友
+     * @var last_index 上一次点击的可见好友的位置
+     * @var step 执行第几步
+     * @var run 运行状态
+     */
+    let abnormal_friends, deleted_abnormal_friends, last_index, step, run;
 
-/**
- * 启动微信
- */
-function launchWeChat() {
-    if (launch(CONFIG.WE_CHAT_PACKAGE_NAME)) {
-    }
-}
-
-/**
- * 点击通讯录
- */
-function clickFriends() {
-    if (NODE_UTIL.backtrackClickNode(text(CONFIG.FRIENDS_TEXT).findOne())) {
-        run = true;
-        return true;
-    }
-    return false;
-}
-
-/**
- * 滚动好友列表
- */
-function scrollFriends() {
-    if (id(CONFIG.FRIEND_LIST_ID).findOne().scrollForward()) {
-        last_index = -1;
-        sleep(500);
-    }
-}
-
-/**
- * 点击好友
- */
-function clickFriend(abnormal_friend) {
-    let friend_nicknames = id(CONFIG.FRIEND_NICKNAME_ID).untilFind();
-    let index = 0;
-    for (let i = 0; i < friend_nicknames.length; i++) {
-        if (friend_nicknames[i].text() == abnormal_friend.friend_nickname) {
-            index = i;
-            break;
+    /**
+     * 启动微信
+     */
+    function launchWeChat() {
+        if (launch(CONFIG.WE_CHAT_PACKAGE_NAME)) {
         }
     }
-    // 跳过连续可见的相同昵称的好友
-    while (index <= last_index) {
-        index++;
+
+    /**
+     * 点击通讯录
+     */
+    function clickFriends() {
+        if (NODE_UTIL.backtrackClickNode(text(CONFIG.FRIENDS_TEXT).findOne())) {
+            run = true;
+            return true;
+        }
+        return false;
     }
-    if (index >= friend_nicknames.length || friend_nicknames[index].text() != abnormal_friend.friend_nickname) {
-        if (id(CONFIG.FRIEND_COUNT_ID).find().empty()) {
-            scrollFriends();
+
+    /**
+     * 滚动好友列表
+     */
+    function scrollFriends() {
+        if (id(CONFIG.FRIEND_LIST_ID).findOne().scrollForward()) {
+            last_index = -1;
+            sleep(800);
+        }
+    }
+
+    /**
+     * 点击好友
+     */
+    function clickFriend(abnormal_friend) {
+        let friend_nicknames = id(CONFIG.FRIEND_NICKNAME_ID).untilFind();
+        let index = 0;
+        for (let i = 0; i < friend_nicknames.length; i++) {
+            if (friend_nicknames[i].text() == abnormal_friend.friend_nickname) {
+                index = i;
+                break;
+            }
+        }
+        // 跳过连续可见的相同昵称的好友
+        while (index <= last_index) {
+            index++;
+        }
+        if (index >= friend_nicknames.length || friend_nicknames[index].text() != abnormal_friend.friend_nickname) {
+            if (id(CONFIG.FRIEND_COUNT_ID).find().empty()) {
+                scrollFriends();
+            } else {
+                stopScript();
+            }
         } else {
-            stopScript();
-        }
-    } else {
-        if (NODE_UTIL.backtrackClickNode(friend_nicknames[index])) {
-            step = 1;
-            last_index = index;
-        }
-    }
-}
-
-/**
-* 检查微信号是否相同
-*/
-function checkWeChatName(abnormal_friend) {
-    if (id(CONFIG.WE_CHAT_NAME_ID).findOne().text() == abnormal_friend.we_chat_name) {
-        step = 2;
-    } else {
-        if (NODE_UTIL.backtrackClickNode(id(CONFIG.BACK_TO_FRIEND_LIST_ID).findOne())) {
-            step = 0;
-        }
-    }
-}
-
-/**
- * 点击更多功能
- */
-function clickMoreFunction() {
-    if (NODE_UTIL.backtrackClickNode(id(CONFIG.MORE_FUNCTION_BY_DELETE_ID).findOne())) {
-        step = 3;
-    }
-}
-
-/**
- * 点击删除功能
- */
-function clickDelete() {
-    let nodes = id(CONFIG.DELETE_ID).untilFind();
-    for (let i = 0; i < nodes.length; i++) {
-        if (nodes[i].text() == CONFIG.DELETE_TEXT) {
-            if (NODE_UTIL.backtrackClickNode(nodes[i])) {
-                step = 4;
-                break;
+            if (NODE_UTIL.backtrackClickNode(friend_nicknames[index])) {
+                step = 1;
+                last_index = index;
             }
         }
     }
-}
 
-/**
- * 点击确认删除
- */
-function clickConfirmDelete(abnormal_friend) {
-    let nodes = id(CONFIG.CONFIRM_DELETE_ID).untilFind();
-    for (let i = 0; i < nodes.length; i++) {
-        if (nodes[i].text() == CONFIG.DELETE_TEXT) {
-            if (NODE_UTIL.backtrackClickNode(nodes[i])) {
-                deleted_abnormal_friends.push(abnormal_friend.we_chat_name);
-                step = 5;
-                last_index--;
-                break;
+    /**
+    * 检查微信号是否相同
+    */
+    function checkWeChatName(abnormal_friend) {
+        if (id(CONFIG.WE_CHAT_NAME_ID).findOne().text() == abnormal_friend.we_chat_name) {
+            step = 2;
+        } else {
+            if (NODE_UTIL.backtrackClickNode(id(CONFIG.BACK_TO_FRIEND_LIST_ID).findOne())) {
+                step = 0;
             }
         }
     }
-}
 
-/**
- * 监听音量下键按下，停止脚本运行
- */
-function keyDownListenerByVolumeDown() {
-    threads.start(function () {
-        toast("按下减小音量键停止脚本运行");
-        // 启用按键监听
-        events.observeKey();
-        events.setKeyInterceptionEnabled("volume_down", true);
-        // 监听音量上键按下
-        events.onceKeyDown("volume_down", function (event) {
-            stopScript();
+    /**
+     * 点击更多功能
+     */
+    function clickMoreFunction() {
+        if (NODE_UTIL.backtrackClickNode(id(CONFIG.MORE_FUNCTION_BY_DELETE_ID).findOne())) {
+            step = 3;
+        }
+    }
+
+    /**
+     * 点击删除功能
+     */
+    function clickDelete() {
+        let nodes = id(CONFIG.DELETE_ID).untilFind();
+        for (let i = 0; i < nodes.length; i++) {
+            if (nodes[i].text() == CONFIG.DELETE_TEXT) {
+                if (NODE_UTIL.backtrackClickNode(nodes[i])) {
+                    step = 4;
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 点击确认删除
+     */
+    function clickConfirmDelete(abnormal_friend) {
+        let nodes = id(CONFIG.CONFIRM_DELETE_ID).untilFind();
+        for (let i = 0; i < nodes.length; i++) {
+            if (nodes[i].text() == CONFIG.DELETE_TEXT) {
+                if (NODE_UTIL.backtrackClickNode(nodes[i])) {
+                    deleted_abnormal_friends.push(abnormal_friend.we_chat_name);
+                    step = 5;
+                    last_index--;
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 监听音量下键按下，停止脚本运行
+     */
+    function keyDownListenerByVolumeDown() {
+        threads.start(function () {
+            // 启用按键监听
+            events.observeKey();
+            events.setKeyInterceptionEnabled("volume_down", true);
+            // 监听音量上键按下
+            events.onceKeyDown("volume_down", function () {
+                stopScript();
+            });
+            toast("按下降低音量键停止脚本运行");
         });
-    });
-}
-
-/**
- * 停止脚本运行
- */
-function stopScript() {
-    run = false;
-    for (let i = 0; i < deleted_abnormal_friends.length; i++) {
-        delete abnormal_friends[deleted_abnormal_friends[i]];
     }
-    COMMON.putAbnormalFriends(abnormal_friends);
-    events.setKeyInterceptionEnabled("volume_down", false);
-    events.removeAllKeyDownListeners("volume_down");
-    toast("脚本已停止运行！！！");
-}
 
-module.exports = {
-    main: () => {
+    /**
+     * 停止脚本运行
+     */
+    function stopScript() {
+        run = false;
+        for (let i = 0; i < deleted_abnormal_friends.length; i++) {
+            delete abnormal_friends[deleted_abnormal_friends[i]];
+        }
+        COMMON.putAbnormalFriends(abnormal_friends);
+        events.setKeyInterceptionEnabled("volume_down", false);
+        events.removeAllKeyDownListeners("volume_down");
+        toast("脚本已停止运行！！！");
+    }
+
+    function main() {
         abnormal_friends = COMMON.getAbnormalFriends(), deleted_abnormal_friends = [], last_index = -1, step = 0, run = false;
         keyDownListenerByVolumeDown();
         launchWeChat();
@@ -189,4 +190,6 @@ module.exports = {
         }
         stopScript();
     }
-}
+
+    return {main: main};
+})();
