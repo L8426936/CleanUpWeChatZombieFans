1d49a9316307902c7b1ffd0495cb01465f62e08f
L8426936 <1278764240@qq.com>
2020-03-01 16:12:16 +0800

    优化微信清理好友脚本的微信版本检查方式

--- clear_friends/utils/app_util.js
+++ clear_friends/utils/app_util.js
@@ -23,32 +23,27 @@ module.exports = {
         let min_supported_version = CONFIG.MIN_SUPPORTED_WE_CHAT_VERSION.match(/\d+/g);
         let current_version = module.exports.getAppVersion(CONFIG.WE_CHAT_PACKAGE_NAME).match(/\d+/g);
         let max_supported_version = CONFIG.MAX_SUPPORTED_WE_CHAT_VERSION.match(/\d+/g);
-        let min_compare_middle = middle_compare_min = middle_compare_max = max_compare_middle = "";
-        for (let i = 0; i < min_supported_version.length || i < current_version.length || i < max_supported_version.length; i++) {
-            let min = parseInt(i < min_supported_version.length ? min_supported_version[i] : 0);
-            let middle = parseInt(i < current_version.length ? current_version[i] : 0);
-            let max = parseInt(i < max_supported_version.length ? max_supported_version[i] : 0);
-            if (min > middle) {
-                min_compare_middle += "3";
-                middle_compare_min += "1";
-            } else if (min == middle) {
-                min_compare_middle += "2";
-                middle_compare_min += "2";
-            } else {
-                min_compare_middle += "1";
-                middle_compare_min += "3";
-            }
-            if (middle > max) {
-                middle_compare_max += "3";
-                max_compare_middle += "1";
-            } else if (middle == max) {
-                middle_compare_max += "2";
-                max_compare_middle += "2";
-            } else {
-                middle_compare_max += "1";
-                max_compare_middle += "3";
+        let min_le_middle = middle_le_max = true;
+        for (let i = 0; i < min_supported_version.length || i < current_version.length; i++) {
+            let min = i < min_supported_version.length ? parseInt(min_supported_version[i]) : 0;
+            let middle = i < current_version.length ? parseInt(current_version[i]) : 0;
+            if (min < middle) {
+                break;
+            } else if (min > middle) {
+                min_le_middle = false;
+                break;
             }
         }
-        return parseInt(min_compare_middle) <= parseInt(middle_compare_min) && parseInt(middle_compare_max) <= parseInt(max_compare_middle);
+        for (let i = 0; i < current_version.length || i < max_supported_version.length; i++) {
+            let middle = i < current_version.length ? parseInt(current_version[i]) : 0;
+            let max = i < max_supported_version.length ? parseInt(max_supported_version[i]) : 0;
+            if (middle < max) {
+                break;
+            } else if (middle > max) {
+                middle_le_max = false;
+                break;
+            }
+        }
+        return min_le_middle && middle_le_max;
     }
 }
